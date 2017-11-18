66b3b04b706797e0824a6ee11a8a40e1ae6de6dc
casp122b@easv365.dk <casp122b@easv365.dk>
2017-11-18 01:50:25 +0100

    Skeleton

---
+++ DAL/Entities/IEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL.Entities
+{
+    interface IEntity
+    {
+        int Id { get; set; }
+    }
+}
---
+++ RestAPI/Controllers/RoomsController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace RestAPI.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Rooms")]
+    public class RoomsController : Controller
+    {
+        // GET: api/Rooms
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Rooms/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+        
+        // POST: api/Rooms
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+        
+        // PUT: api/Rooms/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+        
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
