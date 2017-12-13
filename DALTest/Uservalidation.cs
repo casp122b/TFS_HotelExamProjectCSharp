fac345bf1bb168744015692b4fe1b1ce6a578816
Patrick <mussi_kat@hotmail.com>
2017-12-13 12:55:39 +0100

    Tests

--- DALTest/Uservalidation.cs
+++ DALTest/Uservalidation.cs
@@ -2,29 +2,30 @@
 using System.Text.RegularExpressions;
 using DAL;
 using DAL.Entities;
-using NUnit.Framework;
+using Xunit;
+
 
 
 namespace DALTest
 {
-    [TestFixture]
+    
     public class UservalidationTest
     {
-        [Test]
+        [Fact]
         public void NullTestUser()
         {
             var exception = Assert.Throws<ArgumentException>(
                 () => new UserValidation(null));
-            Assert.AreEqual("User cannot be null", exception.Message);
+            Assert.Equal("User cannot be null", exception.Message);
         }
 
-        [Test]
+        [Fact]
         public void NullTestUserName()
         {
             var user = new User() { Username = null };
             var exception = Assert.Throws<ArgumentException>(
                 () => new UserValidation(user));
-            Assert.AreEqual("Username cannot be null", exception.Message);
+            Assert.Equal("Username cannot be null", exception.Message);
 
         }
      
